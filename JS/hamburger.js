ee428ab941b392d33e66dbdb39ef4e67557458bc
Taka <yukioinokinawa@gmail.com>
2022-07-12 12:04:01 +0900

    fix fix fix!!

--- JS/hamburger.js
+++ JS/hamburger.js
@@ -1,6 +1,6 @@
 jQuery( function( $ ){
     $(".js-hamburger").on("click", function(){
         $(this).toggleClass("is-open");
-        $(".l-nav__body").toggleClass("is-open");
+        $(".l-nav__body").slideToggle("is-open");
     });
 });
